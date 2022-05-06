ea3033f077483cc67c0aaf75c659ce7f9415672a
mkangelo <mikequi000@gmail.com>
2022-05-05 19:47:58 -0500

    movimiento de snake

--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -7,41 +7,58 @@ module Actions
         if position_is_valid?(state, next_position)
             move_snake_to(state, next_position)
         else
-            endgame(state)
+            end_game(state)
         end
-        # si no es valida, terminamos el juego
-        # si es valida movemos la serpiente
+    end
+    private
 
-        private
-
-        def calc_next_position(state)
-            curr_position = state.snake.positions.first
-            case state.next_direction
-                when UP
-                    # decrementar fila
-                    return Model::Coord.new(
-                        curr_position.row -1,
-                        curr_position.col   
-                    ) 
-                when RIGTH
-                    # incrementar col
-                    return Model::Coord.new(
-                        curr_position.row,
-                        curr_position.col + 1
-                    ) 
-                when DOWN
-                    # incrementar fila
-                    return Model::Coord.new(
-                        curr_position.row + 1,
-                        curr_position.col   
-                    ) 
-                when LEFT
-                    # decrementar col
-                    return Model::Coord.new(
-                        curr_position.row,
-                        curr_position.col - 1   
-                    ) 
-            end
+    def calc_next_position(state)
+        curr_position = state.snake.positions.first
+        case state.next_direction
+            when UP
+                # decrementar fila
+                return Model::Coord.new(
+                    curr_position.row -1,
+                    curr_position.col   
+                ) 
+            when RIGTH
+                # incrementar col
+                return Model::Coord.new(
+                    curr_position.row,
+                    curr_position.col + 1
+                ) 
+            when DOWN
+                # incrementar fila
+                return Model::Coord.new(
+                    curr_position.row + 1,
+                    curr_position.col   
+                ) 
+            when LEFT
+                # decrementar col
+                return Model::Coord.new(
+                    curr_position.row,
+                    curr_position.col - 1   
+                ) 
         end
+    end
+
+    def position_is_valid?(state, position)
+        # verificar que este en la grilla
+        is_invalid = ((position.row >= state.grid.rows || position.row < 0) || (position.col >= state.grid.rows || position.col < 0))
+        return false if is_invalid
+
+        # verificar que no este super poniendo a la snake
+        return !(state.snake.positions.include? position)
+    end
+
+    def move_snake_to(state, next_position)
+        new_positions = [next_position] + state.snake.positions[0...-1]
+        state.snake.positions = new_positions
+        state
+    end
+
+    def end_game(state)
+        state.game_finished = true
+        state
     end
 end
